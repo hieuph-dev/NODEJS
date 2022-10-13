c64563e6e9ab5b4209c35bc3af7ed2d00c617693
Hieu Pham <hieupham250201@gmail.com>
2022-10-13 18:57:56 +0700

    upload

--- src/services/userService.js
+++ src/services/userService.js
@@ -110,25 +110,27 @@ let createNewUser = (data) => {
             if(check === true) {
                 resolve({
                     errCode: 1,
-                    message: 'Your email is already in used, Plz try another email'
+                    errMessage: 'Your email is already in used, Plz try another email'
                 })
+            }else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                  email: data.email,
+                  password: hashPasswordFromBcrypt,
+                  firstName: data.firstName,
+                  lastName: data.lastName,
+                  address: data.address,
+                  phoneNumber: data.phoneNumber,
+                  gender: data.gender === "1" ? true : false,
+                  roleId: data.roleId,
+                });
+          
+                resolve({
+                    errCode: 0,
+                    message: 'OK'
+                });
             }
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-              email: data.email,
-              password: hashPasswordFromBcrypt,
-              firstName: data.firstName,
-              lastName: data.lastName,
-              address: data.address,
-              phoneNumber: data.phoneNumber,
-              gender: data.gender === "1" ? true : false,
-              roleId: data.roleId,
-            });
-      
-            resolve({
-                errCode: 0,
-                message: 'OK'
-            });
+            
         } catch(e) {
             reject(e);
         }
