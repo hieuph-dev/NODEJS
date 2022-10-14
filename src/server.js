6dee3302f39b8d391c7f4b6c87c395e31d955598
Hieu Pham <hieupham250201@gmail.com>
2022-10-15 00:14:53 +0700

    upload

--- src/server.js
+++ src/server.js
@@ -3,11 +3,34 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 import connectDB from './config/connectDB.';
-import cors from 'cors';
+// import cors from 'cors';
 require('dotenv').config();
 
 let app = express();
-app.use(cors({ origin: true }));
+// app.use(cors({ origin: true }));
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
+
 //config app
 
 app.use(bodyParser.json());
